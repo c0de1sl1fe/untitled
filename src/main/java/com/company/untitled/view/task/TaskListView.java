89567e9a9d55c0f7dbdaf34defe71c83300dbd0b
every <everydaysmok@gmail.com>
2024-04-23 16:23:31 +0400

    4 and 5 module

---
+++ src/main/java/com/company/untitled/view/task/TaskListView.java
@@ -0,0 +1,16 @@
+package com.company.untitled.view.task;
+
+import com.company.untitled.entity.Task;
+
+import com.company.untitled.view.main.MainView;
+
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "tasks", layout = MainView.class)
+@ViewController("Task_.list")
+@ViewDescriptor("task-list-view.xml")
+@LookupComponent("tasksDataGrid")
+@DialogMode(width = "64em")
+public class TaskListView extends StandardListView<Task> {
+}
