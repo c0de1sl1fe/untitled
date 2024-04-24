4671b4696f379b92f9e99bfed906d967858acf0a
every <everydaysmok@gmail.com>
2024-04-24 08:39:45 +0400

    module: 5 and part of 6

--- src/main/java/com/company/untitled/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/untitled/view/timeentry/TimeEntryDetailView.java
@@ -2,10 +2,14 @@ package com.company.untitled.view.timeentry;
 
 import com.company.untitled.entity.TimeEntry;
 
+import com.company.untitled.entity.User;
 import com.company.untitled.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.core.TimeSource;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
@@ -14,8 +18,16 @@ import java.time.LocalDate;
 @ViewDescriptor("time-entry-detail-view.xml")
 @EditedEntityContainer("timeEntryDc")
 public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
+    @Autowired
+    private TimeSource timeSource;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
     @Subscribe
     public void onInitEntity(final InitEntityEvent<TimeEntry> event) {
-        event.getEntity().setEntryDate(LocalDate.now());
+        event.getEntity().setEntryDate(timeSource.now().toLocalDate());
+        final User user = (User) currentAuthentication.getUser();
+        event.getEntity().setUser(user);
     }
+
 }
